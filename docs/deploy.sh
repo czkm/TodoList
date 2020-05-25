e13867f3e2d8b236b31420975cf24e57d2c7456f
橙汁坤 <36911813+czkm@users.noreply.github.com>
2020-05-26 00:00:42 +0800

    deploy

--- docs/deploy.sh
+++ docs/deploy.sh
@@ -21,6 +21,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+git push -f git@github.com:czkm/TodoList.git master:gh-pages
 
 cd -
