81d4bd6003fe9f81d109b487137f40e73ae71231
橙汁坤 <36911813+czkm@users.noreply.github.com>
2020-05-26 00:37:46 +0800

    dist

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    base:'/TodoList/',
+    base:'/TodoList/dist/',
     title: 'TODOLIST', // 显示在左上角的网页名称以及首页在浏览器标签显示的title名称
     description: 'todolist', // meta 中的描述文字，用于SEO
-    dest:'../dist',
+    dest:'./dist',
     // 注入到当前页面的 HTML <head> 中的标签
     head: [
         ['link', { rel: 'icon', href: '/bg.png' }],  //浏览器的标签栏的网页图标
